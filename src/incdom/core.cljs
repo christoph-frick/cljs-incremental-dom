cd2350c0ee6d61f5eff105ff36832b7ae0df6494
Christoph Frick <rid@ofnir.net>
2015-07-27 22:54:58 +0200

    make behaviour more conforming to sablono; performance improvements; cleanups

--- src/incdom/core.cljs
+++ src/incdom/core.cljs
@@ -1,63 +1,65 @@
 (ns ^:figwheel-always incdom.core
-    (:require))
+    (:require [clojure.string :as s]))
 
 (enable-console-print!)
 
-(defn- elem->tag-name 
+(defn- bench
+  [fn]
+  (let [start (.now js/performance) 
+        result (fn)
+        end (.now js/performance)]
+    (.info js/console (str "Took " (- end start) "ms"))
+    result))
+
+(defn- de-keyword 
   [elem]
   (if (keyword? elem)
     (name elem)
     (str elem)))
 
-; TODO: what's the sane default?
-(defn- decide-static 
-  [kw]
-  (if (= (namespace kw) "static") 
-    :static 
-    :dynamic))
+(defn- extract-classes
+  [elem]
+  (let [[tn & cls] (s/split (de-keyword elem) ".")]
+    [tn (if (empty? cls) {} {:class (s/join " " cls)})]))
 
-(defn- attrs->dynamic-static-attrs 
-  [attrs]
-  (->> attrs
-       (group-by #(decide-static (first %)))
-       (map 
-         (fn [[k vs]] 
-           [k (mapcat (fn [[k v]] 
-                        [(name k) (str v)]) 
-                      vs)]))  
-       (into {})))
+(defn- flatten-attr
+  [a v]
+  (condp (fn [[a-name v-check] [a v]] (and (v-check v) (or (nil? a-name) (= a-name a)))) [a v]
+    [:style map?] (s/join ";" (map (fn [[k v]] (str (de-keyword k) ":" v)) v))
+    [nil fn?] v
+    (str v)))
 
 (defn- apply-incdom 
   [incdom-fn elem attrs]
-  (let [{:keys [static dynamic]} (attrs->dynamic-static-attrs attrs)]
-    (.apply incdom-fn js/IncrementalDOM
-            (into-array (concat 
-                          [(elem->tag-name elem) 
-                           (:key attrs) 
-                           (clj->js static)]
-                          dynamic)))))
+  (let [[tag-name class-map] (extract-classes elem)
+        attrs (merge-with #(str %1 " " %2) attrs class-map) 
+        call-args (reduce 
+                    (fn [a [k v]]
+                      (.push a (name k))
+                      (.push a (flatten-attr k v))
+                      a)
+                    (array tag-name (:key attrs) nil)
+                    attrs)]
+    (.apply incdom-fn js/IncrementalDOM call-args)))
 
 (defn- element-void
-  ([elem]
-   (element-void elem {}))
-  ([elem attrs]
-   (apply-incdom (.-elementVoid js/IncrementalDOM) elem attrs)))
+  [elem attrs]
+  (apply-incdom (.-elementVoid js/IncrementalDOM) elem attrs))
 
 (defn- element-open 
-  ([elem]
-   (element-open elem {}))
-  ([elem attrs]
-   (apply-incdom (.-elementOpen js/IncrementalDOM) elem attrs)))
+  [elem attrs]
+  (apply-incdom (.-elementOpen js/IncrementalDOM) elem attrs))
 
 (defn- element-close
-  ([elem]
-   (.elementClose js/IncrementalDOM elem)))
+  [elem]
+  (.elementClose js/IncrementalDOM elem))
 
-(defn- text [txt]
+(defn- text 
+  [txt]
   (.text js/IncrementalDOM (str txt)))
 
-(defn- patch [root fn]
-  (.patch js/IncrementalDOM root fn))
+(defn patch [root fn]
+  (bench #(.patch js/IncrementalDOM root fn)))
 
 (defn- frest [s] [(first s) (rest s)])
 
@@ -73,8 +75,9 @@
       (do
         (element-open elem attr)
         (doseq [r remainder]
-          (if (keyword? (first r))
-            (hiccup->incremental-dom r)
+          (condp #(%1 %2) r
+            vector? (hiccup->incremental-dom r)
+            sequential? (doseq [rr r] (hiccup->incremental-dom rr))
             (text r)))
         (element-close elem)))))
 
@@ -84,18 +87,18 @@
   (patch (.getElementById js/document "app")
          (fn []
            (hiccup->incremental-dom 
-             [:div {:static/class "main"}
-              [:div ; noattr
+             [:div.main
+              [:div
                "Some plain text"
                " "
                [:strong "Some strong text"]]
-              [:div {:class "row"}
-               [:div {:static/rel-data "test" :class "bold"}
+              [:div.row
+               [:div.bolder {:rel-data "test" :class "bold"}
                 [:label "A Label"]
                 [:input {:static/type "text" :value (count @state)}]]
-               (concat
+               (into
                  [:div {:class "state"}]
-                 (for [d @state] [:div (str d)]))]]))))
+                 (for [d @state] [:div {:style {:font-weight "bold"}} (str d)]))]]))))
 
 (add-watch state :render render!)
 
