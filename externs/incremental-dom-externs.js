0c5b9ab90b89ba82bd422c4c3b237f75ffb5f159
Christoph Frick <rid@ofnir.net>
2016-07-22 11:04:01 +0200

    update to incremental dom 0.4.1

--- externs/incremental-dom-externs.js
+++ externs/incremental-dom-externs.js
@@ -1,9 +1,16 @@
 var IncrementalDOM = {
 	"patch": function () {},
+	"patchOuter": function () {},
+	"patchInner": function () {},
 	"elementVoid": function () {},
 	"elementOpen": function () {},
+	"elementOpenStart": function () {},
+	"elementOpenEnd": function () {},
 	"elementClose": function () {},
+	"attr": function () {},
 	"text": function () {},
 	"skip": function () {},
-	"currentElement": function () {}
+	"skipNode": function () {},
+	"currentElement": function () {},
+	"currentPointer": function () {}
 }
