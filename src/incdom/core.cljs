69eac98635f161ae8e812d061cea9e1955829dc6
Christoph Frick <rid@ofnir.net>
2016-07-22 11:26:54 +0200

    doc strings

--- src/incdom/core.cljs
+++ src/incdom/core.cljs
@@ -42,6 +42,7 @@
          (convert-attr-value attr-value)))
 
 (defn- element-open 
+  "Render an hiccup style opening tag via incdom"
   [elem attrs]
   (let [[tag-name class-map] (extract-classes elem)
         attrs (merge-with #(str %1 " " %2) attrs class-map)]
@@ -50,24 +51,30 @@
     (.elementOpenEnd js/IncrementalDOM tag-name)))
 
 (defn- element-close
+  "Render a closing tag via incdom"
   [elem]
   (.elementClose js/IncrementalDOM elem))
 
 (defn- element-void
+  "Render an empty tag via incdom"
   [elem attrs]
   (do
     (element-open elem attrs)
     (element-close elem)))
 
 (defn- text 
+  "Render an text node via incdom"
   [txt]
   (.text js/IncrementalDOM (str txt)))
 
-(defn patch [root fn]
+(defn patch 
+  "Apply a function, that calls incdom dom manipulations, to a root dom node"
+  [root fn]
   (.patch js/IncrementalDOM root fn))
 
 ; this might not even be close to the capabilities of hiccup
 (defn hiccup->incremental-dom
+  "Run incdom manipulation for a hiccup vector"
   [root]
   (let [[elem & remainder] root
         [attr & remainder] (if (map? (first remainder))
@@ -83,6 +90,8 @@
             (sequential? r) (doseq [rr r] (hiccup->incremental-dom rr))
             :else (text r)))
         (element-close elem)))))
+
+;;; for live testing
 
 (defonce state (atom ()))
 
